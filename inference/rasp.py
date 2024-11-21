a650451f84b84dccf1b42310a77a0f7fe71425eb
Richard <rlla@icomp.ufam.edu.br>
2024-11-21 17:29:15 -0400

    Improve data structures and logic

--- inference/rasp.py
+++ inference/rasp.py
@@ -3,89 +3,105 @@ import mediapipe as mp
 import numpy as np
 import tensorflow as tf
 from picamera2 import Picamera2
-from sklearn.metrics import accuracy_score
+from picamera2.encoders import Encoder
 
-# Carrega o modelo TFLite
-model_path = '../models/model1.tflite'
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
-
-# Pega detalhes do modelo TFLite
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+# Set up the camera
+# 640x480 resolution
+# Null encoder is being used ( i dont know if a encoder could be helpful here, need to check)
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration(main={"format": "RGB888", "size": (640, 480)}, raw={}, encode="raw")
+picam2.configure(video_config)
+encoder = Encoder() # null encoder
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
-# Inicializa a câmera usando Picamera2
-picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)}))
-picam2.start()
 
-frame_buffer = []
-buffer_queda = [0, 0, 0]
+# The model being used is on 16x8 mode
+# The default delegate could not handle this kind of quantization, so Arm nn delegate is being used
+# I need to investigate why the Arm nn delegate is returning warnings
+delegate_lib = tf.lite.experimental.load_delegate('/home/rasp/Downloads/armdelegate/delegate/libarmnnDelegate.so', options={"backends": "CpuRef"})
 
-textoAtual = "Carregando"
-cor = (255, 0, 0)
+# Load the model
+interpreter = tf.lite.Interpreter(model_path='../models/models_tflite/fnet.tflite',
+                                             experimental_delegates=[delegate_lib])
+interpreter.allocate_tensors()
+
+# Input details is basically the expected input shape
+# Output details is the expected outcome
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Begin the recording
+picam2.start_recording(encoder, "test.raw")
+
+frame_buffer = np.empty((1, 44, 132)) # The input of the model is a 3D array
+frame_data = np.empty((33,4)) # This is the structure where the x,y,z and visibility is placed during the capture of the frame
+fall_buffer = [0, 0, 0] 
+
+status = "Loading..."
+color = (255, 0, 0)
+
 
 while True:
-    frame = picam2.capture_array()
-
-    results = pose.process(frame)
+    frame = picam2.capture_array() # current frame
+    results = pose.process(frame) #mediapipe processing the frame
 
     if results.pose_landmarks:
-        landmarks = results.pose_landmarks.landmark
-        frame_data = []
-        for landmark in landmarks:
-            x, y, z, visibility = landmark.x, landmark.y, landmark.z, landmark.visibility
-            frame_data.extend([x, y, z, visibility])
-        frame_buffer.append(frame_data)
+        for i,landmark in enumerate(results.pose_landmarks.landmark): # extraction of landmarks (x,y,z and visibility)
+            frame_data[i] = [landmark.x, landmark.y, landmark.z, landmark.visibility]
+            
+        # Insertion of a new frame_data
+        frame_buffer = np.roll(frame_buffer, -1, axis=1)
+        frame_buffer[0, -1] = frame_data.flatten()
+        frame_data.fill(0)
+        
+        # Is there 44 frames?
+        if np.all(frame_buffer != 0):
+            input_data = frame_buffer.astype(np.float32)
 
-        if len(frame_buffer) > 44:
-            frame_buffer.pop(0)
-            input_data = np.array(frame_buffer).reshape(1, 44, 132).astype(np.float32)
-
-            # Faz a predição com o modelo TFLite
+            # Inference
             interpreter.set_tensor(input_details[0]['index'], input_data)
             interpreter.invoke()
             sensibilidadeDaQueda = interpreter.get_tensor(output_details[0]['index'])[0][0]
 
-            # Verifica a sensibilidade da queda
-            if sensibilidadeDaQueda > 0.99:
-                buffer_queda.pop(0)
-                buffer_queda.append(1)
-                if np.sum(buffer_queda) == len(buffer_queda):
-                    textoAtual = "Queda!"
-                    cor = (0, 0, 255)
+            # Checks the sensitivity to falling
+            if sensibilidadeDaQueda > 0.97:
+                fall_buffer.pop(0)
+                fall_buffer.append(1)
+                if np.sum(fall_buffer) == len(fall_buffer):
+                    status = "Fall!"
+                    color = (0, 0, 255)
             else:
-                buffer_queda.pop(0)
-                buffer_queda.append(0)
-                textoAtual = "Estabilizado"
-                cor = (0, 255, 0)
+                fall_buffer.pop(0)
+                fall_buffer.append(0)
+                status = "Stabilized"
+                color = (0, 255, 0)
 
-            # Desenha os pontos de pose
-            pontosVisiveis = 0
-            for landmark in results.pose_landmarks.landmark:
-                height, width, _ = frame.shape
-                x, y = int(landmark.x * width), int(landmark.y * height)
-                visibility = landmark.visibility
-                if visibility < 0.6:
-                    cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
-                elif visibility < 0.85:
-                    pontosVisiveis += 1
-                    cv2.circle(frame, (x, y), 5, (0, 255, 255), -1)
-                else:
-                    pontosVisiveis += 1
-                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+        # Draw landmarks
+        visible_dots = 0
+        for landmark in results.pose_landmarks.landmark:
+            height, width, _ = frame.shape
+            x, y = int(landmark.x * width), int(landmark.y * height)
+            visibility = landmark.visibility
+            if visibility < 0.6:
+                cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+            elif visibility < 0.85:
+                visible_dots += 1
+                cv2.circle(frame, (x, y), 5, (0, 255, 255), -1)
+            else:
+                visible_dots += 1
+                cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
 
-    # Exibe o status
-    cv2.putText(frame, textoAtual, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, cor, 2)
+    # Status
+    cv2.putText(frame, status, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
-    # Janela de visualização
+    # Window view
     cv2.imshow("Webcam Pose Detection", frame)
 
+    # To exit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-picam2.stop()
+picam2.stop_recording()
 cv2.destroyAllWindows()
